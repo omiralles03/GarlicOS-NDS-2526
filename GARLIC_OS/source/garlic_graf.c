e84770adc969f73b30ed9cfbb8ba69b51241e790
Oupman Miralles <oupman.miralles@estudiants.urv.cat>
2025-10-24 15:35:15 +0200

    progG: _gg_procesarFormnato i _gg_escribir

--- GARLIC_OS/source/garlic_graf.c
+++ GARLIC_OS/source/garlic_graf.c
@@ -78,7 +78,7 @@ void _gg_iniGrafA() {
   lcdMainOnTop();
 
   // Inicialitzacio dels fons BG2 i BG3 en ExRotation 512x512
-  //
+
   // MapBase: 64x64 posicions * 2 bytes/posicio = 8KB
   // Offset mapBase (2KB) = 8/2 = 4
 
@@ -106,36 +106,169 @@ void _gg_iniGrafA() {
   bgUpdate();
 }
 
-/* _gg_procesarFormato: copia los caracteres del string de formato sobre el
-                                          string resultante, pero identifica los
-   códigos de formato precedidos por '%' e inserta la representación ASCII de
-                                          los valores indicados por parámetro.
+/* _gg_procesarFormato: copia los caracteres del string de formato sobre el string 
+                        resultante, pero identifica los códigos de formato precedidos
+                        por '%' e inserta la representación ASCII de los valores 
+                        indicados por parámetro.
         Parámetros:
-                formato	->	string con códigos de formato (ver descripción
-   _gg_escribir); val1, val2	->	valores a transcribir, sean número de
-   código ASCII (%c), un número natural (%d, %x) o un puntero a string (%s);
-                resultado	->	mensaje resultante.
+            formato	    ->	string con códigos de formato (ver descripción _gg_escribir);
+
+            val1, val2	->	valores a transcribir, sean número de código ASCII (%c), 
+                            un número natural (%d, %x) o un puntero a string (%s);
+
+            resultado	->	mensaje resultante.
+
         Observación:
                 Se supone que el string resultante tiene reservado espacio de
-   memoria suficiente para albergar todo el mensaje, incluyendo los caracteres
-                literales del formato y la transcripción a código ASCII de los
-   valores.
+                memoria suficiente para albergar todo el mensaje, incluyendo 
+                los caracteres literales del formato y la transcripción a 
+                código ASCII de los valores.
 */
 void _gg_procesarFormato(char *formato, unsigned int val1, unsigned int val2,
-                         char *resultado) {}
+                         char *resultado) {
+
+    int i = 0;
+    int val_cmp = 0;     // Comptador de valors usats (0=cap, 1=val1, 2=val2)
+    char val_tmp[11];   // Buffer temporal (11 -> 32b + \0)
+
+    for (int j = 0; formato[j] != '\0'; j++) {
+
+        // Copiar caracter literal
+        if (formato[j] != '%') {
+            resultado[i] = formato[j];
+            i++;
+        }
+        // Evaluar codi format
+        else {
+            j++;
+            char codi = formato[j]; // (c, d, x, s, %)
+            
+            // Si no es tracta de % i encara no s'han usat
+            // els dos valors, determinar el valor a usar.
+            unsigned int val_act = 0;
+            if (codi != '%' && val_cmp < 2) { 
+                val_act = (val_cmp == 0) ? val1 : val2;
+            }
+
+            switch (codi) {
+                case 'c':
+                    if (val_cmp < 2) {
+                        resultado[i] = (char)val_act;
+                        i++;
+                        val_cmp++;
+                    }
+                    break;
+
+                // Decimal o Hexa
+                case 'd':
+                case 'x':
+                    // TODO: Mirar si es necessari comprovar el codi
+                    // de retorn de _gs_num2str 0=be, 1=error.
+                    if (val_cmp < 2) {
+                        if (codi == 'd') {
+                            _gs_num2str_dec(val_tmp, sizeof(val_tmp), val_act);
+                        } else { // 'x'
+                            _gs_num2str_hex(val_tmp, sizeof(val_tmp), val_act);
+                        }
+                    }
+                    // Guardar el valor transcrit
+                    for (int k = 0; val_tmp[k] != '\0'; k++) {
+                        if (val_tmp[k] != ' ') { // No guardar espais en blanc
+                            resultado[i] = val_tmp[k];
+                            i++;
+                        }
+                    }
+                    val_cmp++;
+                    break;
+
+                case 's':
+                    if (val_cmp < 2) {
+                        char *string_ptr = (char *)val_act;
+                         
+                        for (int k = 0; string_ptr[k] != '\0'; k++) {
+                            resultado[i] = string_ptr[k];
+                            i++;
+                        }
+                    }
+                    val_cmp++;
+                    break;
+
+                case '%':
+                    resultado[i] = '%';
+                    i++;
+                    break;
+            }
+        }
+    }
+    resultado[i] = '\0';
+}
 
 /* _gg_escribir: escribe una cadena de caracteres en la ventana indicada;
         Parámetros:
                 formato	->	cadena de formato, terminada con centinela '\0';
-                                        admite '\n' (salto de línea), '\t'
-   (tabulador, 4 espacios) y códigos entre 32 y 159 (los 32 últimos son
-   caracteres gráficos), además de códigos de formato %c, %d, %x y %s (max. 2
-   códigos por cadena) val1	->	valor a sustituir en primer código de
-   formato, si existe val2	->	valor a sustituir en segundo código de
-   formato, si existe
-                                        - los valores pueden ser un código ASCII
-   (%c), un valor natural de 32 bits (%d, %x) o un puntero a string (%s) ventana
-   ->	número de ventana (de 0 a 3)
+                            admite '\n' (salto de línea), '\t' (tabulador, 4 espacios) 
+                            y códigos entre 32 y 159 (los 32 últimos son caracteres gráficos), 
+                            además de códigos de formato %c, %d, %x y %s (max. 2 códigos por cadena)
+
+                val1	->	valor a sustituir en primer código de formato, si existe 
+
+                val2	->	valor a sustituir en segundo código de formato, si existe
+                            - los valores pueden ser un código ASCII (%c), 
+                              un valor natural de 32 bits (%d, %x)
+                              o un puntero a string (%s)
+
+                ventana ->	número de ventana (de 0 a 3)
 */
 void _gg_escribir(char *formato, unsigned int val1, unsigned int val2,
-                  int ventana) {}
+                  int ventana) {
+     // Maxim 3 linies de ventana
+    char resultado[3 * VCOLS];
+
+    _gg_procesarFormato(formato, val1, val2, resultado); 
+
+    // 16 bits altos: número de línea
+    int filaAct = _gd_wbfs[ventana].pControl >> 16;
+    // 16 bits bajos: caracteres pendientes
+    int numChars = _gd_wbfs[ventana].pControl & 0xFFFF;
+
+    int i = 0;
+    char character;
+
+    while ((character = resultado[i]) != '\0') {
+        // Tabulacio
+        if (character == '\t') {
+            // Escriure espais fins la proxima columna en pos % 4
+            do {
+                _gd_wbfs[ventana].pChars[numChars] = ' ';
+                numChars++;
+            } while ((numChars < VCOLS) && (numChars % 4 != 0));
+        }
+        // Salt de linia o buffer ple
+        else if (character == '\n' || numChars == VCOLS) {
+            _gp_WaitForVBlank();
+            // Scroll si arribem al final
+            if (filaAct == VFILS - 1) {
+                _gg_desplazar(ventana);
+            }
+            // Escriure Linia i reiniciar numChars
+            _gg_escribirLinea(ventana, filaAct, numChars);
+            numChars = 0;
+            // Avancar fila si NO estem al final
+            if (filaAct < VFILS - 1) {
+                filaAct++;
+            }
+        }
+        // Caracter literal
+        else {
+            _gd_wbfs[ventana].pChars[numChars] = character;
+            numChars++;
+        }
+
+        // Actualitzar pControl per el proxim _gg_escribir()
+        // Posar num linia a 16 bits alts 
+        // OR per 16 bits baixos dels caracters pendents
+        _gd_wbfs[ventana].pControl = (filaAct << 16) | numChars;
+
+        i++; // seguent caracter
+    }
+}
