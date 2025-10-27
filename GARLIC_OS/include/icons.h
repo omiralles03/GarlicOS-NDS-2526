fe3d5fa06c11fd99dc8ec9da33eb427317cc4127
Oupman Miralles <oupman.miralles@estudiants.urv.cat>
2025-10-27 18:18:44 +0100

    progG: grit correcte dels sprites

---
+++ GARLIC_OS/include/icons.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(icons)
+
+//======================================================================
+//
+//	icons, 2048x32@8, 
+//	Transparent palette entry: 3.
+//	+ palette 256 entries, not compressed
+//	+ 1024 tiles Metatiled by 4x4 lz77 compressed
+//	Total size: 512 + 32584 = 33096
+//
+//	Time-stamp: 2025-10-27, 18:09:44
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.14
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_ICONS_H
+#define GRIT_ICONS_H
+
+#define iconsTilesLen 32584
+extern const unsigned int iconsTiles[8146];
+
+#define iconsPalLen 512
+extern const unsigned short iconsPal[256];
+
+#endif // GRIT_ICONS_H
+
+//}}BLOCK(icons)
