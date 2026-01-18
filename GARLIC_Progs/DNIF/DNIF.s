687dfa9f6432f7799ca6c19d6ba45db2de7f6e44
Oupman Miralles <oupman.miralles@estudiants.urv.cat>
2026-01-18 16:13:10 +0100

    progG: garlic progs files

--- GARLIC_Progs/DNIF/DNIF.s
+++ GARLIC_Progs/DNIF/DNIF.s
@@ -39,19 +39,55 @@ random_inRange:
 	.section	.rodata
 	.align	2
 .LC0:
-	.ascii	"-- Programa DNIF  -  PID (%d) --\012\000"
+	.ascii	"%0-- Programa DNIF  -  PID (%d) --\012\000"
 	.align	2
 .LC1:
-	.ascii	"TRWAGMYFPDXBNJZSQVHLCKE\000"
+	.ascii	"DNI\000"
 	.align	2
 .LC2:
-	.ascii	"\012NIE: %c-%d\000"
+	.ascii	"NIE\000"
 	.align	2
 .LC3:
-	.ascii	"%c\012\012\000"
+	.ascii	"\012%0Argument rebut: %2%d %0(%3%s%0)\012\000"
 	.align	2
 .LC4:
-	.ascii	"\012DNI: %d-%c\012\012\000"
+	.ascii	"TRWAGMYFPDXBNJZSQVHLCKE\000"
+	.align	2
+.LC5:
+	.ascii	"\012%0Random Digit NIE: %2%d\012\000"
+	.align	2
+.LC6:
+	.ascii	"%0Random Generat: %2%d\012\000"
+	.align	2
+.LC7:
+	.ascii	"%0Numero NIE: %2%d\012\000"
+	.align	2
+.LC8:
+	.ascii	"\012%0Random DNI Generat: %2%d\012\000"
+	.align	2
+.LC9:
+	.ascii	"%0Residu (%1%d%0 %% %123%0) = %2%d\012\000"
+	.align	2
+.LC10:
+	.ascii	"%0Lletres[%1%s%0]\012\000"
+	.align	2
+.LC11:
+	.ascii	"%0Lletra DNI: Lletres[%1%d%0] = %2%c\012\000"
+	.align	2
+.LC12:
+	.ascii	"%0digitoNIE[X=0, Y=1, Z>1]: %2%d\012\000"
+	.align	2
+.LC13:
+	.ascii	"\012%0NIE resultant: %2%c-%d\000"
+	.align	2
+.LC14:
+	.ascii	"%c\012\012\000"
+	.align	2
+.LC15:
+	.ascii	"\012%0DNI resultant: %2%d\000"
+	.align	2
+.LC16:
+	.ascii	"-%c\012\012\000"
 	.text
 	.align	2
 	.global	_start
@@ -65,10 +101,7 @@ _start:
 	str	lr, [sp, #-4]!
 	sub	sp, sp, #44
 	str	r0, [sp, #4]
-	bl	GARLIC_pid
-	mov	r3, r0
-	mov	r1, r3
-	ldr	r0, .L14
+	ldr	r0, .L16
 	bl	GARLIC_printf
 	ldr	r3, [sp, #4]
 	cmp	r3, #1
@@ -80,34 +113,59 @@ _start:
 	mov	r3, #0
 	str	r3, [sp, #4]
 .L5:
-	ldr	r3, .L14+4
+	ldr	r3, [sp, #4]
+	cmp	r3, #0
+	bne	.L6
+	ldr	r3, .L16+4
+	b	.L7
+.L6:
+	ldr	r3, .L16+8
+.L7:
+	mov	r2, r3
+	ldr	r1, [sp, #4]
+	ldr	r0, .L16+12
+	bl	GARLIC_printf
+	ldr	r3, .L16+16
 	str	r3, [sp, #28]
 	ldr	r3, [sp, #4]
 	cmp	r3, #0
-	beq	.L6
+	beq	.L8
 	mov	r1, #0
 	mov	r0, #3
 	bl	random_inRange
 	mov	r3, r0
 	strh	r3, [sp, #34]	@ movhi
-	ldr	r1, .L14+8
-	ldr	r0, .L14+12
+	ldr	r1, .L16+20
+	ldr	r0, .L16+24
 	bl	random_inRange
 	str	r0, [sp, #24]
 	ldrsh	r3, [sp, #34]
-	ldr	r2, .L14+16
+	ldr	r2, .L16+28
 	mul	r3, r2, r3
 	mov	r2, r3
 	ldr	r3, [sp, #24]
 	add	r3, r2, r3
 	str	r3, [sp, #36]
-	b	.L7
-.L6:
-	ldr	r1, .L14+16
-	ldr	r0, .L14+20
+	ldrsh	r3, [sp, #34]
+	mov	r1, r3
+	ldr	r0, .L16+32
+	bl	GARLIC_printf
+	ldr	r1, [sp, #24]
+	ldr	r0, .L16+36
+	bl	GARLIC_printf
+	ldr	r1, [sp, #36]
+	ldr	r0, .L16+40
+	bl	GARLIC_printf
+	b	.L9
+.L8:
+	ldr	r1, .L16+28
+	ldr	r0, .L16+44
 	bl	random_inRange
 	str	r0, [sp, #36]
-.L7:
+	ldr	r1, [sp, #36]
+	ldr	r0, .L16+48
+	bl	GARLIC_printf
+.L9:
 	add	r3, sp, #16
 	add	r2, sp, #12
 	mov	r1, #23
@@ -118,58 +176,89 @@ _start:
 	add	r3, r2, r3
 	ldrb	r3, [r3]
 	strb	r3, [sp, #23]
+	ldr	r3, [sp, #16]
+	mov	r2, r3
+	ldr	r1, [sp, #36]
+	ldr	r0, .L16+52
+	bl	GARLIC_printf
+	ldr	r1, [sp, #28]
+	ldr	r0, .L16+56
+	bl	GARLIC_printf
+	ldr	r3, [sp, #16]
+	ldrb	r2, [sp, #23]	@ zero_extendqisi2
+	mov	r1, r3
+	ldr	r0, .L16+60
+	bl	GARLIC_printf
 	ldr	r3, [sp, #4]
 	cmp	r3, #0
-	beq	.L8
+	ble	.L10
 	ldrsh	r3, [sp, #34]
 	cmp	r3, #0
-	bne	.L9
+	bne	.L11
 	mov	r3, #88
 	strh	r3, [sp, #32]	@ movhi
-.L9:
+.L11:
 	ldrsh	r3, [sp, #34]
 	cmp	r3, #1
-	bne	.L10
+	bne	.L12
 	mov	r3, #89
 	strh	r3, [sp, #32]	@ movhi
-	b	.L11
-.L10:
+	b	.L13
+.L12:
 	mov	r3, #90
 	strh	r3, [sp, #32]	@ movhi
-.L11:
+.L13:
+	ldrsh	r3, [sp, #34]
+	mov	r1, r3
+	ldr	r0, .L16+64
+	bl	GARLIC_printf
 	ldrsh	r3, [sp, #32]
 	ldr	r2, [sp, #36]
 	mov	r1, r3
-	ldr	r0, .L14+24
+	ldr	r0, .L16+68
 	bl	GARLIC_printf
 	ldrb	r3, [sp, #23]	@ zero_extendqisi2
 	mov	r1, r3
-	ldr	r0, .L14+28
+	ldr	r0, .L16+72
 	bl	GARLIC_printf
-	b	.L12
-.L8:
-	ldrb	r3, [sp, #23]	@ zero_extendqisi2
-	mov	r2, r3
+	b	.L14
+.L10:
 	ldr	r1, [sp, #36]
-	ldr	r0, .L14+32
+	ldr	r0, .L16+76
 	bl	GARLIC_printf
-.L12:
+	ldrb	r3, [sp, #23]	@ zero_extendqisi2
+	mov	r1, r3
+	ldr	r0, .L16+80
+	bl	GARLIC_printf
+.L14:
 	ldrb	r3, [sp, #23]	@ zero_extendqisi2
 	mov	r0, r3
 	add	sp, sp, #44
 	@ sp needed
 	ldr	pc, [sp], #4
-.L15:
+.L17:
 	.align	2
-.L14:
+.L16:
 	.word	.LC0
 	.word	.LC1
-	.word	1000000
-	.word	9000000
-	.word	10000000
-	.word	90000000
 	.word	.LC2
 	.word	.LC3
 	.word	.LC4
+	.word	1000000
+	.word	9000000
+	.word	10000000
+	.word	.LC5
+	.word	.LC6
+	.word	.LC7
+	.word	90000000
+	.word	.LC8
+	.word	.LC9
+	.word	.LC10
+	.word	.LC11
+	.word	.LC12
+	.word	.LC13
+	.word	.LC14
+	.word	.LC15
+	.word	.LC16
 	.size	_start, .-_start
 	.ident	"GCC: (devkitARM release 46) 6.3.0"
